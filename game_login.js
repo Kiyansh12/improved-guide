6a48fbaf066bc53dc632d6aea581a39deee16e5d
Kiyansh12 <142411681+Kiyansh12@users.noreply.github.com>
2024-04-17 19:18:58 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function addUser() {
+  window.location = "index.html";
+  player1_name = document.getElementById("player1_name").value;
+  player2_name = document.getElementById("player2_name").value;
+}
+
+
+
+
